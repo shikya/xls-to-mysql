8d52511b97a52d00e0d62d334e7620bb327246b0
Shrikant Sonone <shrikantsonone@gmail.com>
2015-09-09 15:52:51 +0530

    batch detect

--- xlsmysql.java
+++ xlsmysql.java
@@ -3,11 +3,36 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
+class Batch{
+    public int year;
+    public String batch;
+    public String batchname;
 
+    public Batch(int year, String batch, String batchname) {
+        this.year = year;
+        this.batch = batch;
+        this.batchname = batchname;
+    }
 
-public class xlsmysql{
+}
+
+class Student{
+    public int afg;
+    public String name;
+
+    @Override
+    public String toString() {
+        return "student{" + "afg=" + afg + ", name=" + name + '}';
+    }
+    
+}
+
+class Students extends ArrayList<Student>{
+    
+}
+class xlsmysql{
     
     public static String stripQuotes(String str){
         StringBuilder sb=new StringBuilder();
@@ -28,13 +53,40 @@ public class xlsmysql{
         String line = br.readLine();
         StringTokenizer st;
         int errorcount=0;
+        int studentcounter =1;
+        boolean flag = false;
         while((line=br.readLine())!=null){
+            //System.out.println("Parsing : "+line);
+            line = stripQuotes(line);
             line = line.trim();
+            
+            flag=false;
+            for (StringTokenizer stringTokenizer = new StringTokenizer(line); stringTokenizer.hasMoreTokens();) {
+                String token = stringTokenizer.nextToken();
+                if(token.equalsIgnoreCase("Batch")){
+                    flag = true;
+                    
+                }
+            }
+
             st= new StringTokenizer(line);
-            int afgno=0;
+            Student student = new Student();
+            
             try {
-                afgno = Integer.parseInt(st.nextToken());
-                //System.out.println(afgno + " >> "+ line);
+                student.afg = Integer.parseInt(st.nextToken());
+                student.name = "";
+                while(st.hasMoreTokens()){
+                    student.name+=st.nextToken() + " ";
+                }
+                student.name = (student.name).trim();
+                System.out.println();
+                while(studentcounter < student.afg){
+                    //System.out.print(" \n" + student.afg + " ");
+                    studentcounter++;
+                }
+                System.out.print(student.toString());
+
+                //System.out.println(student.afg + " >> "+ line);
             } catch (Exception e) {
                 if(line.length()==0)
                     continue;
@@ -43,6 +95,7 @@ public class xlsmysql{
                 line = line.trim();
                 System.out.println(" >"+line+"<");
             }
+
         }
         System.out.println("Errors = "+errorcount);
     }
